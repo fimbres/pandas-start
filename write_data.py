eae449baa6cfced6372882de8052166746b74eb1
Isaac Fimbres <fimbresisaac23@gmail.com>
2023-05-06 15:53:17 -0700

    changing the file

--- write_data.py
+++ write_data.py
@@ -4,12 +4,14 @@ data = pd.read_csv("./data/financial-report.csv")
 
 # writing data
 
+print(data.head())
+
+data = data.drop(columns=['Series_reference']) # deleting columns
+
+print(data.head())
+
 data['New column'] = data['Period'] * 25 # adding a new column
 
 print(data.head()) # printing the new file
-
-data = data.drop(columns=['New column']) # deleting columns
-
-print(data.head())
 
 data.to_csv('./data/updated-report.csv') # saving the changes
